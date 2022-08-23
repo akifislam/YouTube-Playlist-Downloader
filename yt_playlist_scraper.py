ca3b64a7589401ce8514dcecad6c6c1f55df73ee
Akif Islam <iamakifislam@gmail.com>
2022-08-23 21:41:44 +0600

    fix user_input

--- yt_playlist_scraper.py
+++ yt_playlist_scraper.py
@@ -1,7 +1,6 @@
 from pytube import YouTube
 from pytube import Playlist
 import os
-import moviepy.editor as mp
 import re
 
 # Parsing YouTube Playlist URL
--- yt_to_mp3_downloader.py
+++ yt_to_mp3_downloader.py
@@ -2,10 +2,11 @@ from yt_playlist_scraper import get_songs
 from converter import download_mp3
 
 #Playlist URL
-YOUTUBE_PLAYLIST_URL = input("Enter the URL of your playlist") or 'https://www.youtube.com/playlist?list=PLB-0AGqg4hO99EoYUGyaR4H-1zBqN5wvi'
+YOUTUBE_PLAYLIST_URL = input("Enter the URL of your playlist :\n") or 'https://www.youtube.com/playlist?list=PLB-0AGqg4hO99EoYUGyaR4H-1zBqN5wvi'
 
 #Enter the directory address where you want to save songs
-download_directory = "/Users/akifislam/Desktop/YouTubePlaylistDownloader" or '.'
+download_directory = input("Enter the directory where you want to save (or simply put a '.' if you want to ignore):\n") or '.'
+print("Please wait ...")
 
 songs = get_songs(YOUTUBE_PLAYLIST_URL) #Enter the playlist link you want to download
 
